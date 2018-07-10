e3288a91d60eb87d22c5499d196080a6ffe4d303
Vbobell <vinicius.bobel41@gmail.com>
2018-07-10 00:10:15 -0300

    variables example
    Created representation file to variables examples.

---
+++ variables.js
@@ -0,0 +1,13 @@
+/**
+Variáveis são representações dinâmicas de uma propriedade ou dado.
+Em javascript possuímos três tipos de declaração para váriaveis:
+@argument var representa uma váriavel de escopo global ou de escopo, dependendo de onde é declarada.
+@argument let representa uma váriavel somente de escopo de onde é declarada.
+@argument const representa uma váriavel constante que não muda após ser definida.
+*/
+
+var variable;
+let escopeVariable;
+const constantVariable;
+
+

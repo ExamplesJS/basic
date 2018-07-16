31f5321d6f9307dbe7a20ad50396469f6b130e73
RENAN.BASTOS <renanbastos.tec@gmail.com>
2018-07-16 09:55:17 -0300

    improved code indentation

--- variables.js
+++ variables.js
@@ -1,39 +1,40 @@
 /**
-@description
-Variáveis são representações dinâmicas de uma propriedade ou dado.
-Em javascript possuímos três tipos de declaração para váriaveis:
-@argument var @description
-representa uma váriavel de escopo global ou de função, dependendo de onde é declarada.
-@argument let @description
-representa uma váriavel somente de escopo de onde é declarada.
-@argument const @description
-representa uma váriavel constante que não muda após ser definida, uma constante tem que ser definida.
+    @description
+        Variáveis são representações dinâmicas de uma propriedade ou dado.
+        Em javascript possuímos três tipos de declaração para váriaveis:
+    @argument var @description
+        representa uma váriavel de escopo global ou de função, dependendo de onde é declarada.
+    @argument let @description
+        representa uma váriavel somente de escopo de onde é declarada.
+    @argument const @description
+        representa uma váriavel constante que não muda após ser definida, uma constante tem que ser definida.
 
-@description Ao adicionar o sinal de =  ao lado da váriavel, 
-é atribuído na váriavel o valor que for definido.
-@example 
-    var variable = 1; 
-    let escopeVariable = 2;
-    const constantVariable = 3;
+    @description 
+        Ao adicionar o sinal de =  ao lado da váriavel, 
+        é atribuído na váriavel o valor que for definido.
+    @example 
+        var variable = 1; 
+        let escopeVariable = 2;
+        const constantVariable = 3;
 **/
 
 /** 
-@example @argument var 
-Váriavel @var variable declarada dentro de um objeto é visível fora do seu escopo. 
+    @example @argument var 
+    Váriavel @var variable declarada dentro de um objeto é visível fora do seu escopo. 
 **/
     {
         var variable = 1;
     }
     console.log(variable); // 1
 /**
-Atribuir novo valor na váriavel.
+    Atribuir novo valor na váriavel.
 **/
     variable = 2;
     console.log(variable); // 2
 
 /**
-@example @argument let
-Váriavel @var escopeVariavel declarada dentro de um objeto não é visível fora do seu escopo. 
+    @example @argument let
+    Váriavel @var escopeVariavel declarada dentro de um objeto não é visível fora do seu escopo. 
 **/
     let escopeVariable;
     {
@@ -41,19 +42,18 @@ Váriavel @var escopeVariavel declarada dentro de um objeto não é visível for
     }
     console.log(escopeVariable); // undefined
 /**
-Atribuí valor a váriavel 
+    Atribuí valor a váriavel 
 **/
     escopeVariable = 1;
     console.log(variable); // 1
 
 /**
-@example @argument const
-Váriavel @const constantVariable uma vez declarada não será possível alterar o seu valor. 
+    @example @argument const
+    Váriavel @const constantVariable uma vez declarada não será possível alterar o seu valor. 
 **/
     const constantVariable = 1;
     console.log(constantVariable); // 1
 /**
-Ao atribuir novo valor a constante, aparecerá um erro.
+    Ao atribuir novo valor a constante, aparecerá um erro.
     constantVariable = 2; // error: Assignment to constant variable.
 **/
-
